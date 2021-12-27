c81674fa19a04d6307a0435755874f484648d35d
Jonata Ramalho <jonataramalho.s@gmail.com>
2021-12-27 20:17:01 -0300

    Feat: Adiciona Rotas

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import { ThemeProvider } from "styled-components";
 import { Reset } from "styled-reset";
 
 import theme from "./theme";
+import Routes from "./routes";
 
 function App() {
   return (
     <ThemeProvider theme={theme}>
       <Reset />
+      <Routes />
     </ThemeProvider>
   );
 }
