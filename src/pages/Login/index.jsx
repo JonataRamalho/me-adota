1249fcb262939397d67312e37314dc9a2b2a37eb
Jonata Ramalho <jonataramalho.s@gmail.com>
2021-12-30 22:21:50 -0300

    Feat: Cria página login

---
+++ src/pages/Login/index.jsx
@@ -0,0 +1,82 @@
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { Background, Header } from "../../components";
+
+import {
+  Main,
+  Input,
+  Form,
+  Button,
+  ForgotPassword,
+  Title,
+  Container,
+  Image,
+  ButtonBack,
+} from "./styles";
+import arrowLeft from "../../assets/arrow-left.svg";
+
+const Login = () => {
+  const [page, setPage] = useState(0);
+
+  const navigate = useNavigate();
+
+  function showLogin() {
+    return (
+      <Main>
+        <Form onSubmit={handleLogin}>
+          <Input placeholder="Email" type="email" />
+          <Input placeholder="Senha" type="password" />
+
+          <Button type="submit">Entrar</Button>
+          <ForgotPassword onClick={() => setPage(1)}>
+            Esqueceu a Senha?
+          </ForgotPassword>
+        </Form>
+      </Main>
+    );
+  }
+
+  function showForgotPassword() {
+    return (
+      <Main>
+        <Container>
+          <ButtonBack onClick={() => setPage(0)}>
+            <Image src={arrowLeft} /> Voltar
+          </ButtonBack>
+        </Container>
+        <Title>Coloque seu email para enviarmos uma nova senha</Title>
+        <Form>
+          <Input placeholder="Email" type="email" />
+          <Button type="submit">Enviar</Button>
+        </Form>
+      </Main>
+    );
+  }
+
+  function renderPage() {
+    if (page === 0) {
+      return showLogin();
+    } else if (page === 1) {
+      return showForgotPassword();
+    }
+  }
+
+  function handleLogin(e) {
+    e.preventDefault();
+
+    try {
+      navigate("/profile");
+    } catch (err) {
+      alert("Falha no login, tente novamente.");
+    }
+  }
+
+  return (
+    <Background>
+      <Header />
+      {renderPage()}
+    </Background>
+  );
+};
+
+export default Login;
---
+++ src/pages/Login/styles.js
@@ -0,0 +1,122 @@
+import styled from "styled-components";
+
+export const Main = styled.main`
+  /* background-color: beige; */
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+
+  margin-top: 60px;
+`;
+
+export const Form = styled.form`
+  /* background-color: bisque; */
+  width: 100%;
+  max-width: 350px;
+
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
+
+export const Input = styled.input`
+  width: 272px;
+  height: 48px;
+
+  margin-top: 16px;
+  padding-left: 16px;
+
+  border-radius: 8px;
+  outline: none;
+  border: none;
+  background-color: ${(props) => props.theme.colors.input};
+  color: ${(props) => props.theme.colors.text};
+
+  font-family: ${(props) => props.theme.fonts.montserrat};
+  font-size: 14px;
+  font-weight: 500;
+
+  ::-webkit-input-placeholder {
+    color: ${(props) => props.theme.colors.tertiary};
+  }
+`;
+
+export const Button = styled.button`
+  width: 290px;
+  height: 48px;
+
+  border-radius: 8px;
+  border: none;
+  cursor: pointer;
+  background-color: ${(props) => props.theme.colors.primary};
+  color: ${(props) => props.theme.colors.secondary};
+
+  margin-top: 16px;
+
+  font-family: ${(props) => props.theme.fonts.montserrat};
+  font-size: 16px;
+  font-weight: bold;
+
+  transition: 0.2s ease-in;
+
+  &:hover {
+    background-color: ${(props) => props.theme.colors.background};
+  }
+`;
+
+export const ForgotPassword = styled.a`
+  margin-top: 16px;
+  cursor: pointer;
+  color: ${(props) => props.theme.colors.tertiary};
+
+  font-family: ${(props) => props.theme.fonts.montserrat};
+  font-size: 14px;
+  font-weight: 500;
+
+  transition: 0.2s ease-in;
+
+  &:hover {
+    color: ${(props) => props.theme.colors.detail};
+  }
+`;
+
+export const Title = styled.h1`
+  font-family: ${(props) => props.theme.fonts.montserrat};
+  font-size: 22px;
+  font-weight: 300;
+
+  margin-bottom: 24px;
+`;
+
+export const Container = styled.div`
+  /* background-color: brown; */
+
+  width: 100%;
+  max-width: 736px;
+
+  margin-bottom: 48px;
+  /* display: flex;
+  flex-direction: column;
+  align-items: center; */
+`;
+
+export const Image = styled.img`
+  width: 32px;
+  height: 32px;
+`;
+
+export const ButtonBack = styled.a`
+  cursor: pointer;
+
+  color: ${(props) => props.theme.colors.tertiary};
+
+  font-family: ${(props) => props.theme.fonts.montserrat};
+  font-size: 18px;
+  font-weight: bold;
+
+  transition: 0.2s ease-in;
+
+  display: flex;
+
+  align-items: center;
+`;
