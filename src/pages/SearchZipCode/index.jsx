b2cfc068d1c5098f1e63b63d5bf439b434a0eb22
Jonata Ramalho <jonataramalho.s@gmail.com>
2022-01-09 01:51:26 -0300

    Feat: Cria página pesquisar cep

---
+++ src/pages/SearchZipCode/index.jsx
@@ -0,0 +1,21 @@
+import { Background, Header, Title } from "../../components";
+import { Button, ContainerTile, Form, Input, Main } from "./styles";
+
+const SearchZipCode = () => {
+  return (
+    <Background>
+      <Header type={2} />
+      <Main>
+        <ContainerTile>
+          <Title content="Precisamos do seu cep para facilitar a nossa pesquisa 😁" />
+        </ContainerTile>
+        <Form>
+          <Input placeholder="Ex: 57084040" />
+          <Button to="/pets">Pesquisar</Button>
+        </Form>
+      </Main>
+    </Background>
+  );
+};
+
+export default SearchZipCode;
