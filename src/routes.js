c34043a44b360268a5e9ba6703ac88651557bdbf
Jonata Ramalho <jonataramalho.s@gmail.com>
2022-01-09 00:23:57 -0300

    Feat: Cria rota pets cadastrados

--- src/routes.js
+++ src/routes.js
@@ -5,6 +5,7 @@ import {
   Options,
   PasswordRecovery,
   Register,
+  RegisteredPets,
   RegisterPet,
 } from "./pages";
 
@@ -18,6 +19,7 @@ const Routes = () => {
         <Route path="/register" element={<Register />} />
         <Route path="/options" element={<Options />} />
         <Route path="/registerPet" element={<RegisterPet />} />
+        <Route path="/petsAdoption" element={<RegisteredPets />} />
       </Switch>
     </BrowserRouter>
   );
