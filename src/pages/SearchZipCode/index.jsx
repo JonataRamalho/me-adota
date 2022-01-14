222bcb1c9794ed4bbb58fcf5de33a13d837a58c3
Jonata Ramalho <jonataramalho.s@gmail.com>
2022-01-13 22:10:46 -0300

    Fix: Busca e salva cep do usuário

--- src/pages/SearchZipCode/index.jsx
+++ src/pages/SearchZipCode/index.jsx
@@ -1,7 +1,34 @@
+import { useEffect, useState } from "react";
 import { Background, Header, Title } from "../../components";
 import { Button, ContainerTile, Form, Input, Main } from "./styles";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import cepApi from "cep-promise";
 
 const SearchZipCode = () => {
+  const [cepUser, setCepUser] = useState("");
+
+  async function getCep() {
+    try {
+      const address = await cepApi(cepUser);
+
+      const { cep, state, city, neighborhood } = address;
+
+      const dataAddress = {
+        zip_code: cep,
+        state,
+        city,
+        neighborhood,
+      };
+
+      localStorage.setItem("@storage_Address", JSON.stringify(dataAddress));
+    } catch (error) {
+      toast.error("CEP não encontrado! ", {
+        position: toast.POSITION.TOP_CENTER,
+      });
+    }
+  }
+
   return (
     <Background>
       <Header type={2} />
@@ -10,10 +37,18 @@ const SearchZipCode = () => {
           <Title content="Precisamos do seu cep para facilitar a nossa pesquisa 😁" />
         </ContainerTile>
         <Form>
-          <Input placeholder="Ex: 57084040" />
-          <Button to="/pesquisar/pets">Pesquisar</Button>
+          <Input
+            placeholder="Ex: 57084040"
+            value={cepUser}
+            onChange={(e) => setCepUser(e.target.value)}
+            maxLength={8}
+          />
+          <Button to="/pesquisar/pets" onClick={() => getCep()}>
+            Pesquisar
+          </Button>
         </Form>
       </Main>
+      <ToastContainer />
     </Background>
   );
 };
