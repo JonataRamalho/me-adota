400cb219fcda0d4f53e12f0820dac8aced770545
Jonata Ramalho <jonataramalho.s@gmail.com>
2023-05-21 14:35:06 -0300

    fix: Cria container para dashboards

--- src/components/Container/styles.js
+++ src/components/Container/styles.js
@@ -10,3 +10,23 @@ export const ContainerContentCustom = styled.div`
   flex-direction: column;
   align-items: center;
 `;
+
+export const ContainerDashboardCustom = styled.div`
+  background-color: ${(props) => props.theme.colors.secondary};
+  width: 400px;
+  height: 256px;
+
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
+  padding: 16px;
+  box-shadow: rgba(0, 0, 0, 0.1) 0px 0px 5px 0px,
+    rgba(0, 0, 0, 0.1) 0px 0px 1px 0px;
+  border-radius: 16px;
+
+  @media (max-width: 1366px) {
+    width: 360px;
+    height: 216px;
+  }
+`;
